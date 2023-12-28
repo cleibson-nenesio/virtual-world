7b55fbba129aee3c88527c3748d2d0a0bc588bf6
Cleibson Nenesio <cleibson.nenesio@hotmail.com>
2023-12-28 16:58:46 -0300

    first commit

---
+++ js/math/graph.js
@@ -0,0 +1,91 @@
+class Graph {
+	constructor(points = [], segments = []) {
+		this.points = points;
+		this.segments = segments;
+	}
+
+	static load(info) {
+		const points = info.points.map((point) => new Point(point.x, point.y));
+		const segments = info.segments.map(
+			(segment) =>
+				new Segment(
+					points.find((p) => p.equals(segment.p1)),
+					points.find((p) => p.equals(segment.p2))
+				)
+		);
+
+		return new Graph(points, segments);
+	}
+
+	hash() {
+		return JSON.stringify(this);
+	}
+
+	addPoint(point) {
+		this.points.push(point);
+	}
+
+	containsPoint(point) {
+		return this.points.find((p) => p.equals(point));
+	}
+
+	tryAddPoint(point) {
+		if (!this.containsPoint(point)) {
+			this.addPoint(point);
+			return true;
+		}
+
+		return false;
+	}
+
+	removePoint(point) {
+		const segments = this.getSegmentsWithPoint(point);
+
+		for (const segment of segments) {
+			this.removeSegment(segment);
+		}
+
+		this.points.splice(this.points.indexOf(point), 1);
+	}
+
+	addSegment(segment) {
+		this.segments.push(segment);
+	}
+
+	containsSegment(segment) {
+		return this.segments.find((s) => s.equals(segment));
+	}
+
+	tryAddSegment(segment) {
+		if (!this.containsSegment(segment) && !segment.p1.equals(segment.p2)) {
+			this.addSegment(segment);
+
+			return true;
+		}
+
+		return false;
+	}
+
+	removeSegment(segment) {
+		this.segments.splice(this.segments.indexOf(segment), 1);
+	}
+
+	getSegmentsWithPoint(point) {
+		return this.segments.filter((s) => s.includes(point));
+	}
+
+	dispose() {
+		this.points.length = 0;
+		this.segments.length = 0;
+	}
+
+	draw(ctx) {
+		for (const seg of this.segments) {
+			seg.draw(ctx);
+		}
+
+		for (const point of this.points) {
+			point.draw(ctx);
+		}
+	}
+}
